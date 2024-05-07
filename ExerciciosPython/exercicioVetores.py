025af322c42ebbdd041a73f7f0bd5485f4dfb717
ichcamile <84331711+ichcamile@users.noreply.github.com>
2024-05-07 23:14:51 +0000

    feat: adc exericio1 - 7/5

---
+++ ExerciciosPython/exercicioVetores.py
@@ -0,0 +1,14 @@
+# exercicios dessa aula
+
+def exercicio1():
+# 1.Crie um programa que leia 10 valores inteiros em um vetor e, em seguida, mostre na tela os valores lidos.
+    valoresArray = []
+    
+    for i in range(0,10):
+        valor = input("Digite um dos valores!")
+        valoresArray.append(valor)
+    
+    print(valoresArray)
+
+def exercicio2():
+   # Ler um conjunto de números reais, armazenando-o em vetor e calcular o quadrado dos componentes deste vetor, armazenando o resultadoem outro vetor. Os conjuntos tem 20 elementos cada. Imprimir todas as listas.
