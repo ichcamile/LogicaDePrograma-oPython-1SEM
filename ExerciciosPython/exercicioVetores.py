d143f185b081b9e851f7d9487e6eb59b82d330de
ichcamile <84331711+ichcamile@users.noreply.github.com>
2024-05-13 01:46:18 +0000

    feat: adc exericios

--- ExerciciosPython/exercicioVetores.py
+++ ExerciciosPython/exercicioVetores.py
@@ -13,4 +13,20 @@ def exercicio1():
 def exercicio2():
    # Ler um conjunto de números reais, armazenando-o em vetor e calcular o quadrado dos componentes deste vetor, armazenando o resultadoem outro vetor. Os conjuntos tem 20 elementos cada. Imprimir todas as listas.
 
-    
+    valoresReais = []
+    valoresQuadrado = []
+    quantidadeValores = int(input("Quantos números você quer no conjunto?"))
+
+    for i in range(0,quantidadeValores):
+        valor = int(input("Digite um dos valores: "))
+        valoresReais.append(valor)
+        valoresQuadrado.append(valor * valor) 
+
+    print(valoresReais)
+    print(valoresQuadrado)
+
+def exercicio3():
+    #Faça um programa que leia um vetor de 18 posições e, em seguida, leia também 2 valores (X e Y) quaisquer correspondentes a 2 posições no vetor. Ao final seu programa deverá escrever a soma dos valores encontrados nas respectivas posições X e Y
+
+def exercicio4():
+    #Leia um vetor de 10 posições. Contar e escrever quantos valores pares ele possui
